6da458a038051a5b18cc80808e6fc08aba4b6a31
Romit Karmakar <romitkarmakar@gmail.com>
2020-01-13 18:34:11 +0530

    Added eurekoin api

--- app.js
+++ app.js
@@ -1,23 +1,64 @@
 require("dotenv").config();
 const express = require("express");
+const utf8 = require("utf8");
 const app = express();
-const Config = require("./models/Config");
+var sha1 = require("sha1");
+const axios = require("axios");
 
 app.use(require("cors")());
 app.use(express.json());
 
-app.use("/sevenup", require("./controllers/sevenupController"));
-app.use("/profile", require("./controllers/profileController"));
+function generateToken(email) {
+  var encodeString = utf8.encode(`${email}${process.env.SECRET_KEY}`);
+  console.log(sha1(encodeString));
+  return sha1(encodeString);
+}
 
-app.get("/install", async function(req, res) {
-  var config = Config({
-    type: "store",
-    value: 0
-  });
+app.get("/", function(req, res) {
+  res.send("Hello from server");
+});
 
-  await config.save();
+app.post("/create", async function(req, res) {
+  let receiver = req.body.email;
+  let sender = process.env.GAME_EMAIL;
 
-  res.send("Installed Successfully");
+  if (req.body.status == "loser") {
+    receiver = [sender, (sender = receiver)][0];
+  }
+
+  var response = await axios.get(
+    `${process.env.EUREKOIN_BASE_URL}api/transfer/${generateToken(
+      sender
+    )}?amount=10&email=${receiver}`
+  );
+
+  if (response.data.status == "0")
+    res.send({
+      message: "Status updated"
+    });
+  else res.send("ERROR");
+});
+
+app.post("/coins", function(req, res) {
+  axios
+    .get(
+      `${process.env.EUREKOIN_BASE_URL}api/coins/?token=${generateToken(
+        req.body.email
+      )}`
+    )
+    .then(response => res.send(response.data))
+    .catch(err => res.send(err));
+});
+
+// Handle 404 error
+app.use(function(req, res, next) {
+  return res.status(404).send({ status: "404", message: "Api doesn't exists" });
+});
+// Handle 500 error
+app.use(function(err, req, res, next) {
+  return res
+    .status(500)
+    .send({ status: "404", message: "Server Error", error: err });
 });
 
 app.listen(process.env.PORT, () =>
